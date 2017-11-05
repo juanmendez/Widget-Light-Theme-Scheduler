7186c7d2fc5b2373cc1d50908178a0e435ce6e13
Juan Mendez <contact@juanmendez.info>
2017-11-05 16:57:03 -0600

    some small bug found in main activity

--- lightthemedemo/app/src/main/java/info/juanmendez/lightthemedemo/MainActivity.java
+++ lightthemedemo/app/src/main/java/info/juanmendez/lightthemedemo/MainActivity.java
@@ -91,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }else{
+            notifyThemeManager();
         }
     }
 
